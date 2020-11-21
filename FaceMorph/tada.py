c6c13efbc70f117f265e9784e0884f2761f4f9b3
Alcindo João <joao.az-rib@hotmail.com>
2020-11-21 20:55:03 -0300

    Códigos e imagens utilizados na aplicação

---
+++ FaceMorph/tada.py
@@ -0,0 +1,74 @@
+import cv2
+import numpy as np
+import dlib
+
+# Load the detector
+detector = dlib.get_frontal_face_detector()
+
+# Load the predictor
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+# read the image
+img = cv2.imread("strang2.jpg")
+
+    
+
+# Convert image into grayscale
+gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+
+# Use detector to find landmarks
+faces = detector(gray)
+for face in faces:
+    x1 = face.left() # left point
+    y1 = face.top() # top point
+    x2 = face.right() # right point
+    y2 = face.bottom() # bottom point
+
+    # Create landmark object
+    landmarks = predictor(image=gray, box=face)
+
+    
+    points = []
+    # Loop through all the points
+    for n in range(0, 68):
+        x = landmarks.part(n).x
+        y = landmarks.part(n).y
+    
+        
+        points.append([x,y])
+    
+    points_bordo = [[377 ,190],[280, 400],[0, 390],[599 ,390],[0, 0],[0 ,190],[0 ,799],[300, 799],[599, 799],[599, 190],[599, 0],[300, 0]]
+    
+    for i in points_bordo:
+        points.append(i)
+    
+    for n in points:
+        x = n[0]
+        y = n[1]
+        
+        # Draw a circle
+        cv2.circle(img=img, center=(x, y), radius=3, color=(0,0, 255), thickness=-1)
+
+def write_txt(conteudo, filename):
+      points = []
+      for i in range(len(conteudo)):
+          if i != (len(conteudo)-1):
+              points.append(f"{conteudo[i][0]} {conteudo[i][1]}\n")
+          else:
+              points.append(f"{conteudo[i][0]} {conteudo[i][1]}")
+           
+      arquivo = open(filename + ".txt", 'w') 
+      arquivo.writelines(points)
+      arquivo.close()
+     
+write_txt(points, "strang2.jpg")
+
+# show the image
+cv2.imshow(winname="Face", mat=img)
+
+# Delay between every fram
+cv2.waitKey(delay=0)
+
+# Close all windows
+cv2.destroyAllWindows()
+
---
+++ FaceMorph/triangulacao.py
@@ -0,0 +1,90 @@
+import cv2
+import numpy as np
+import random
+
+# Check if a point is inside a rectangle
+def rect_contains(rect, point) :
+    if point[0] < rect[0] :
+        return False
+    elif point[1] < rect[1] :
+        return False
+    elif point[0] > rect[2] :
+        return False
+    elif point[1] > rect[3] :
+        return False
+    return True
+
+# Draw a point
+def draw_point(img, p, color ) :
+    cv2.circle( img, p, 2, color, 0 )
+
+
+# Draw delaunay triangles
+def draw_delaunay(img, subdiv, delaunay_color ) :
+
+    triangleList = subdiv.getTriangleList();
+    size = img.shape
+    r = (0, 0, size[1], size[0])
+    
+    
+    for t in triangleList :
+        
+        pt1 = (t[0], t[1])
+        pt2 = (t[2], t[3])
+        pt3 = (t[4], t[5])
+        
+        if rect_contains(r, pt1) and rect_contains(r, pt2) and rect_contains(r, pt3) :
+        
+            cv2.line(img, pt1, pt2, delaunay_color, 1, 0)
+            cv2.line(img, pt2, pt3, delaunay_color, 1,  0)
+            cv2.line(img, pt3, pt1, delaunay_color, 1,  0)
+
+
+
+
+if __name__ == '__main__':
+
+    # Define window names
+    win_delaunay = "Delaunay Triangulation"
+
+    
+    # Define colors for drawing.
+    delaunay_color = (255,255,255)
+    points_color = (0, 0, 255)
+
+    # Read in the image.
+    img = cv2.imread("bonoro.jpg");
+    
+    # Rectangle to be used with Subdiv2D
+    size = img.shape
+    rect = (0, 0, size[1], size[0])
+    
+    # Create an instance of Subdiv2D
+    subdiv = cv2.Subdiv2D(rect);
+
+    # Create an array of points.
+    points = [];
+    
+    # Read in the points from a text file
+    with open("bonoro.jpg.txt") as file :
+        for line in file :
+            x, y = line.split()
+            points.append((int(x), int(y)))
+    
+    for p in points :
+        subdiv.insert(p)
+          
+
+    # Draw delaunay triangles
+    draw_delaunay( img, subdiv, (0, 0, 255) );
+
+    # Draw points
+    for p in points :
+        draw_point(img, p, (0,0,255))
+
+
+
+    # Show results
+    cv2.imshow(win_delaunay,img)
+    cv2.waitKey(0)
+
